27629ac329549428659d89b00caaefb42481d358
Manall21 <90984159+Manall21@users.noreply.github.com>
2023-04-27 22:46:50 +0200

    Delete kledingstuk directory

--- kledingstuk/src/main/java/be/odisee/kledingstuk/dao/KledingstukRepository.java
+++
@@ -1,8 +0,0 @@
-package be.odisee.kledingstuk.dao;
-
-import be.odisee.kledingstuk.domain.Kledingstuk;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KledingstukRepository extends JpaRepository<Kledingstuk, Integer> {
-
-}
